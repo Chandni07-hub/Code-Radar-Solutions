271e85fabdbbff8c72ca6d6fe16db550b82c4141
Chandni <chandni0985@gmail.com>
2025-02-13 23:16:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    char op;
-    double a, b, res;
-
-    // Read the operator
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &op);
-
-    // Read the two numbers
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &a, &b);
-   
-    switch (op) {
-    case '+':
-        res = a + b;
-        break;
-    case '-':
-        res = a - b;
-        break;
-    case '*':
-         res = a * b;
-        break;
-    case '/':
-        res = a / b;
-        break;
-    default:
-        printf("Error! Incorrect Operator Value\n");
-        res = -DBL_MAX;
+// Function to perform the calculation
+double calculate(double num1, double num2, char operator) {
+    double result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            // Check for division by zero
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+                return 0;
+            }
+            break;
+        default:
+            printf("Error! Operator is not correct.\n");
+            return 0;
     }
-    if(res!=-DBL_MAX)
-      printf("%.2lf", res);
-    
+    return result;
+}
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    // First calculation: 5 + 3
+    num1 = 5;
+    operator = '+';
+    num2 = 3;
+    printf("Result of %.2lf %c %.2lf: %.2lf\n", num1, operator, num2, calculate(num1, num2, operator)); // Output: 8
+
+    // Second calculation: 10 / 2
+    num1 = 10;
+    operator = '/';
+    num2 = 2;
+    printf("Result of %.2lf %c %.2lf: %.2lf\n", num1, operator, num2, calculate(num1, num2, operator)); // Output: 5
+
     return 0;
 }
