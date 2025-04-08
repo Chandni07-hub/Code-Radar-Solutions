5308a0135a6bbb800bb4b1ba8226b310f4192e98
Chandni <chandni0985@gmail.com>
2025-04-08 11:38:37 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -1,17 +1,14 @@
-// Your code here...
-int findKthMissing(int arr[], int n, int k) {
-    int missingCount = 0, current = 1, index = 0;
+#include<stdio.h>
+#include<string.h>
 
-    while (missingCount < k) {
-        if (index < n && arr[index] == current) {
-            index++; // Skip the number that exists in the array
-        } else {
-            missingCount++; // Count this as a missing number
-            if (missingCount == k) {
-                return current; // Return the K-th missing number
-            }
-        }
-        current++; // Move to the next number
+int main(){
+    char str[100];
+    scanf("%s",str);
+
+    int len = stren(str);
+    for(int i = len-1;i>=0;i--){
+        printf("%c",str[i]);
     }
-    return -1; // This line should not be reached
+printf("\n");
+return 0;
 }
