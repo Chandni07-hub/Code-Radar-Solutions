1faff8b26cf5a81af40a6eb1d60a922d7553ff88
Chandni <chandni0985@gmail.com>
2025-04-08 12:01:05 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <ctype.h>
 
-int cmp(const void *a, const void *b) {
-    return (char)a - (char)b;
-}
+#define CHAR_RANGE 256
 
-int main() {
-    char a[100], b[100];
-    scanf("%s %s", a, b);
+int areAnagrams(char str1[], char str2[]) {
+    int count[CHAR_RANGE] = {0};
+    int i;
 
-    if (strlen(a) != strlen(b)) {
-        printf("No\n");
+    // If lengths are not equal, they can't be anagrams
+    if (strlen(str1) != strlen(str2)) {
         return 0;
     }
 
-    qsort(a, strlen(a), sizeof(char), cmp);
-    qsort(b, strlen(b), sizeof(char), cmp);
+    // Count frequency of each character
+    for (i = 0; str1[i] && str2[i]; i++) {
+        count[(unsigned char)str1[i]]++;
+        count[(unsigned char)str2[i]]--;
+    }
 
-    printf(strcmp(a, b) == 0 ? "Yes\n" : "No\n");
-    return 0;
-}
+    // If all counts are zero, they are anagrams
+    for (i = 0; i < CHAR_RANGE; i++) {
+        if (count[i] != 0) {
+            return 0
