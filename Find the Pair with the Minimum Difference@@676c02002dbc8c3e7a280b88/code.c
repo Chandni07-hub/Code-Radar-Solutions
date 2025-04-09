e8fbc93d09d2d7f34ee028749858c7ff6c87bf86
Chandni <chandni0985@gmail.com>
2025-04-09 12:39:42 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Comparison function for qsort
+// Compare function for qsort
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
+
 int main() {
     int n;
     scanf("%d", &n);
-
     int arr[n];
+
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    // Sort the array
+
     qsort(arr, n, sizeof(int), compare);
 
-    // Find the pair with the smallest difference
     int minDiff = abs(arr[1] - arr[0]);
     int first = arr[0], second = arr[1];
 
     for(int i = 1; i < n - 1; i++) {
         int diff = abs(arr[i + 1] - arr[i]);
-        if(diff < minDiff) {
+
+        if(diff < minDiff || (diff == minDiff && arr[i] < first)) {
             minDiff = diff;
             first = arr[i];
             second = arr[i + 1];
@@ -30,6 +31,5 @@ int main() {
     }
 
     printf("%d %d\n", first, second);
-
     return 0;
 }
