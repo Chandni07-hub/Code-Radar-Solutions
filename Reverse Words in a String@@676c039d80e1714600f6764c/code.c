037c4107b568774fb686025968fcea18b8b3cd81
Chandni <chandni0985@gmail.com>
2025-04-09 11:19:44 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
 void reverseWord(char *start, char *end) {
     while (start < end) {
         char temp = *start;
-        *start++ = *end;
-        *end-- = temp;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
     }
 }
-int main() {
-    char str[1000];
-    fgets(str, sizeof(str), stdin);
-    int len = strlen(str);
-    if (str[len - 1] == '\n') str[len - 1] = '\0';
-    char *word_start = str;
-    for (int i = 0; ; i++) {
-        if (str[i] == ' ' || str[i] == '\0') {
-            reverseWord(word_start, &str[i - 1]);
-            if (str[i] == '\0')
-                break;
-            word_start = &str[i + 1];
+
+void reverseWords(char *str) {
+    char *wordStart = NULL;
+    char *temp = str;
+
+    while (*temp) {
+        if (wordStart == NULL && *temp != ' ') {
+            wordStart = temp;
         }
+
+        if (wordStart && (*(temp + 1) == ' ' || *(temp + 1) == '\0')) {
+            reverseWord(wordStart, temp);
+            wordStart = NULL;
+        }
+
+        temp++;
     }
+}
+
+int main() {
+    char str[100];
+
+
+    fgets(str, sizeof(str), stdin);
+
+    // Remove the newline character added by fgets
+    str[strcspn(str, "\n")] = '\0';
+
+   
+
+    reverseWords(str);
+
     printf("%s\n", str);
+
     return 0;
 }
