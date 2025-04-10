28694c528c82a1ec9041a4a76fc94066754df72d
Chandni <chandni0985@gmail.com>
2025-04-10 15:55:14 +0530

    Solution to Find the Leaders in an Array

---
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+// Function to check if a number is prime
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i]))
+            count++;
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
